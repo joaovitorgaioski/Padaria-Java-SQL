c650820390b2d79e6a5cf7266b97633fcd54b003
João Vitor Gaioski <jvgaioskI@hotmail.com>
2026-05-07 14:29:47 -0300

    Novas features

--- src/main/java/com/github/joao/util/MessageHelper.java
+++ src/main/java/com/github/joao/util/MessageHelper.java
@@ -1,12 +1,15 @@
 package com.github.joao.util;
 
 import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextInputDialog;
+
+import java.util.Optional;
 
 public class MessageHelper {
 
     /**
      * Método auxiliar para exibir mensagens dinamicamente
-     *
      * @param tipo      Tipo da mensagem
      * @param titulo    Título
      * @param cabecalho Cabecalho
@@ -23,5 +26,28 @@ public class MessageHelper {
 
     public static void mostrarMensagem(Alert.AlertType tipo, String titulo, String mensagem) {
         mostrarMensagem(tipo, titulo, null, mensagem);
+    }
+
+    public static boolean confirmar(String titulo, String cabecalho, String mensagem) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle(titulo);
+        alert.setHeaderText(cabecalho);
+        alert.setContentText(mensagem);
+
+        Optional<ButtonType> result = alert.showAndWait();
+        return result.isPresent() && result.get() == ButtonType.OK;
+    }
+
+    public static String inputMessage(String titulo, String cabecalho, String mensagem) {
+        TextInputDialog dialog = new TextInputDialog();
+        dialog.setTitle(titulo);
+        dialog.setContentText(mensagem);
+
+        Optional<String> result = dialog.showAndWait();
+        return result.orElse(null);
+    }
+
+    public static String inputMessage(String titulo, String mensagem) {
+        return inputMessage(titulo, null, mensagem);
     }
 }
